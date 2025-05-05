4f01d603f6d008dabe9a112f5b973e90507b0fe1
your-username <your-email@example.com>
2025-05-05 18:27:58 +0530

    add filles

---
+++ Client/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Navbar from './Component/Navbar';
+
+function App() {
+  return (
+    <div>
+      <Navbar />
+      {/* Other components */}
+    </div>
+  );
+}
+
+export default App;
