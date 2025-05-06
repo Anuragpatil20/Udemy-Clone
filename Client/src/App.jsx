e8b0c7b151424a6f8195c8971a231be3570a7530
your-username <your-email@example.com>
2025-05-06 12:35:13 +0530

    added new files

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import Navbar from './Component/Navbar';
+import Home from './Component/Home';
 
 function App() {
   return (
     <div>
       <Navbar />
+      <Home/>
       {/* Other components */}
     </div>
   );
